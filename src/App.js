c4c550d69f25e909f6d4f3d4da20273f900e6b8d
Lee Chuan Xin <chuanxin.lee@gmail.com>
2017-03-16 01:07:34 +0800

    Init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    };
+  };
+
+  update(evt) {
+    let code = evt.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+        .transform(code, {presets: ['es2015', 'react']})
+        .code,
+        err: ''
+      });
+    }
+    catch(err) {
+      this.setState({err: err.message});
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update.bind(this)} defaultValue={this.state.input} />
+          <pre>{this.state.output}</pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
